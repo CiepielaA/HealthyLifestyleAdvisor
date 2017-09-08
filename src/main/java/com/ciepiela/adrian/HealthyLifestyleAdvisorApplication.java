a0f8073c958f8fc81e66194b4f10a351cd7bbef8
CiepielaAdrian <adrianciepiela@o2.pl>
2017-09-08 19:49:41 +0200

    initialize spring-boot project

---
+++ src/main/java/com/ciepiela/adrian/HealthyLifestyleAdvisorApplication.java
@@ -0,0 +1,12 @@
+package com.ciepiela.adrian;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HealthyLifestyleAdvisorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HealthyLifestyleAdvisorApplication.class, args);
+	}
+}
