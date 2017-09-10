30e11f9f9654b6ac60119e83f2af04c5226e93a0
CiepielaAdrian <adrianciepiela@o2.pl>
2017-09-10 15:14:55 +0200

    update User model class

--- src/main/java/com/ciepiela/adrian/model/User.java
+++ src/main/java/com/ciepiela/adrian/model/User.java
@@ -14,7 +14,43 @@ public class User {
     private String login;
 
     @Column(nullable = false)
+    private String password;
+
+    @Column(nullable = false)
     private String email;
 
+    public User() {
+    }
 
+    public long getUserId() {
+        return userId;
+    }
+
+    public void setUserId(long userId) {
+        this.userId = userId;
+    }
+
+    public String getLogin() {
+        return login;
+    }
+
+    public void setLogin(String login) {
+        this.login = login;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
 }
