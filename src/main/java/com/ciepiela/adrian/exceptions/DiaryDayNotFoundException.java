206df57d17d9f05331d7337647b37a4d08d97c86
CiepielaAdrian <adrianciepiela@o2.pl>
2017-10-21 16:24:24 +0200

    add: DiaryDayNotFoundException ProductNotFoundException

---
+++ src/main/java/com/ciepiela/adrian/exceptions/DiaryDayNotFoundException.java
@@ -0,0 +1,22 @@
+package com.ciepiela.adrian.exceptions;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
+import java.time.LocalDate;
+
+@ResponseStatus(HttpStatus.NOT_FOUND)
+public class DiaryDayNotFoundException extends RuntimeException {
+    private final static Logger LOGGER = LoggerFactory.getLogger(DiaryDayNotFoundException.class);
+
+    public DiaryDayNotFoundException(long diaryDayId) {
+        LOGGER.warn("Could not find diaryDay with id: {}", diaryDayId);
+    }
+
+    public DiaryDayNotFoundException(LocalDate date, long userId) {
+        LOGGER.warn("Could not find diaryDay with date: {} and userId: {}", date, userId);
+    }
+
+}
---
+++ src/main/java/com/ciepiela/adrian/exceptions/ProductNotFoundException.java
@@ -0,0 +1,20 @@
+package com.ciepiela.adrian.exceptions;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
+@ResponseStatus(HttpStatus.NOT_FOUND)
+public class ProductNotFoundException {
+
+    private final static Logger LOGGER = LoggerFactory.getLogger(ProductNotFoundException.class);
+
+    public ProductNotFoundException(long productId) {
+        LOGGER.warn("Could not find product with id: {}", productId);
+    }
+
+    public ProductNotFoundException(String description) {
+        LOGGER.warn("Could not find product with description: {}", description);
+    }
+}
