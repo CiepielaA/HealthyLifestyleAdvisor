ec4ad5044238fb92acbee9326b0426ab0312efaa
CiepielaAdrian <adrianciepiela@o2.pl>
2017-10-21 16:11:28 +0200

    add methods to DairyDay and ProductRepository

--- src/main/java/com/ciepiela/adrian/dao/DairyDayRepository.java
+++ src/main/java/com/ciepiela/adrian/dao/DairyDayRepository.java
@@ -4,7 +4,10 @@ import com.ciepiela.adrian.model.DiaryDay;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.time.LocalDate;
+import java.util.Optional;
 
 public interface DairyDayRepository extends JpaRepository<DiaryDay, Long> {
-    DiaryDay findByDateAndUser (LocalDate date, long userId);
+    Optional<DiaryDay> findByDiaryDayId (long diaryDayId);
+
+    Optional<DiaryDay> findByDateAndUser (LocalDate date, long userId);
 }
--- src/main/java/com/ciepiela/adrian/dao/ProductRepository.java
+++ src/main/java/com/ciepiela/adrian/dao/ProductRepository.java
@@ -3,6 +3,10 @@ package com.ciepiela.adrian.dao;
 import com.ciepiela.adrian.model.Product;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface ProductRepository extends JpaRepository<Product, Long> {
-    Product findByDescription (String description);
+    Optional<Product> findByProductId (long productId);
+
+    Optional<Product> findByDescription (String description);
 }
